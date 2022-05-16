ce0a45b40cdcff8fc3376f85e61d2b65c25be97f
liujunjie <liujunjie@beisen.com>
2022-05-16 17:55:28 +0800

    add SlidingWindowsTemplete

---
+++ SlidingWindows/SlidingWindowsTemplete.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.SlidingWindows
+{/// <summary>
+/// 基于LongestOnes的SlidingWindows模板
+/// </summary>
+    public class SlidingWindowsTemplete
+    {
+        public int SlidingWindows(int[] A, int K)
+        {
+            int left = 0, right = 0, zeroCounter = 0;
+            while (right < A.Length)
+            {
+                if (A[right] == 0) zeroCounter++;
+                if (zeroCounter > K)
+                {
+                    left++;
+                    if (A[left - 1] == 0) zeroCounter++;
+                }
+                right++;
+            }
+            return right - left;
+        }
+
+    }
+}

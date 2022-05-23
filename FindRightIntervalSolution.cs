859795dee97021ac40a80930c698ce65bbce1010
liujunjie <liujunjie@beisen.com>
2022-05-23 18:50:16 +0800

    Add FindRightIntervalSolutions

---
+++ FindRightIntervalSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class FindRightIntervalSolution
+    {
+        public int[] FindRightInterval(int[][] intervals)
+        {
+            int[] result = new int[intervals.Length];
+            
+            return result;
+        }
+    }
+}

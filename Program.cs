707451e1d906930989998a546c99ce74b2ad920b
liujunjie <liujunjie@eastmoney.com>
2022-07-13 12:41:10 +0800

    init in EM

--- Program.cs
+++ Program.cs
@@ -7,26 +7,30 @@ using Algorithm.DFS;
 using Algorithm.BFS;
 
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //TwoSumSolution.Instance.TwoSum(new int[]{1,2},1);
 
-//TwoSumSolution.Instance.TwoSum(new int[]{1,2},1);
+        //AddTwoNumbersSolution.Instance.AddTwoNumbers(new ListNode().CreatLinkedListByInsertToHead(), new ListNode().CreatLinkedListByInsertToHead());
 
-//AddTwoNumbersSolution.Instance.AddTwoNumbers(new ListNode().CreatLinkedListByInsertToHead(), new ListNode().CreatLinkedListByInsertToHead());
+        //LengthOfLongestSubstringSolution.Instance.LengthOfLongestSubstring("zbacdeeeac");
 
-//LengthOfLongestSubstringSolution.Instance.LengthOfLongestSubstring("zbacdeeeac");
+        //FindMaxConsecutiveOnesSolution.Instance.FindMaxConsecutiveOnes(new int[] {1,1,0,1,1,1});
 
-//FindMaxConsecutiveOnesSolution.Instance.FindMaxConsecutiveOnes(new int[] {1,1,0,1,1,1});
+        //NumSubarrayProductLessThanKSolution.Instance.NumSubarrayProductLessThanK(new int[] { 1, 2, 3 }, 0);
 
-//NumSubarrayProductLessThanKSolution.Instance.NumSubarrayProductLessThanK(new int[] { 1, 2, 3 }, 0);
+        //LongestOnesSolution.Instance.LongestOnes(new int[] { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }, 3);
 
-//LongestOnesSolution.Instance.LongestOnes(new int[] { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }, 3);
+        //FindMedianSortedArraysSolution.Instance.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
 
-//FindMedianSortedArraysSolution.Instance.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
+        //LargestValuesSolution.Instance.LargestValues(new TreeNode (1,null,null));
 
-//LargestValuesSolution.Instance.LargestValues(new TreeNode (1,null,null));
+        //FindBottomLeftValueSolution.Instance.FindBottomLeftValue(new TreeNode(1, new TreeNode(2,null,null), new TreeNode(1,null,null)));
 
-//FindBottomLeftValueSolution.Instance.FindBottomLeftValue(new TreeNode(1, new TreeNode(2,null,null), new TreeNode(1,null,null)));
-
-
+    }
+}
 
 
 
